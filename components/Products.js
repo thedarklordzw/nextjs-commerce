2c05ee8804dc1a93fcd8733de61ad76e41b483ac
Ulathi Ncube <swiftly.graphics@gmail.com>
2023-01-30 02:15:32 +0200

    new link colors

--- components/Products.js
+++ components/Products.js
@@ -1,22 +1,32 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import styles from './Products.module.css';
 import Card from './Card';
 import Link from 'next/link';
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  getDoc,
+} from 'firebase/firestore';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import { getStorage, ref } from 'firebase/storage';
 
+const productIds = [];
 const Products = () => {
   const [data, setData] = useState('');
+
   const [error, setError] = useState(false);
   const [images, setImages] = useState('');
 
   const [trending, setTrending] = useState('');
   const [onSale, setOnSale] = useState('');
   const [forSale, setForSale] = useState('');
+
+  const clickedElementRef = useRef();
 
   const firebaseConfig = {
     apiKey: 'AIzaSyBxSHrV27jbVguscEki_YSHkDxxULgSS0o',
@@ -42,13 +52,31 @@ const Products = () => {
         setTrending(trending);
         setForSale(forSale);
         setOnSale(onSale);
-
-        console.dir(`${doc.id} => ${doc.data()}`);
       });
     };
 
     getData();
   }, []);
+
+  const getDocHandler = async () => {
+    const docRef = doc(db, 'stock', 'inStock');
+    const docSnap = await getDoc(docRef);
+
+    const clickedElement = clickedElementRef;
+
+    if (docSnap.exists()) {
+      const { trending: trendingData } = docSnap.data();
+
+      for (const product of productIds) {
+        const productId = trendingData.find(
+          productName => productName.id === product
+        );
+        console.log(productId);
+      }
+    } else {
+      console.log('No such document');
+    }
+  };
 
   return (
     <div>
@@ -60,25 +88,36 @@ const Products = () => {
               <section className={styles['on-sale']}>
                 <h2 className='heading-secondary'>Trending Items</h2>
                 <ul className={styles.products}>
-                  {trending.map(product => (
-                    <motion.li key={product.id} className={styles.item}>
-                      <article className={styles.product}>
-                        <Image src={product.image} height={300} width={300} />
-                        <p className={styles.price}>
-                          <span>{product.item}</span>
-                          <span>{product.price}</span>
-                        </p>
-                        <button className={styles.order}>Add to Cart</button>
-                      </article>
-                    </motion.li>
-                  ))}
+                  {trending.map(product => {
+                    productIds.push(product.id);
+                    return (
+                      <motion.li
+                        onClick={getDocHandler}
+                        key={product.id}
+                        className={styles.item}
+                      >
+                        <article className={styles.product}>
+                          <Image src={product.image} height={300} width={300} />
+                          <p className={styles.price}>
+                            <span>{product.item}</span>
+                            <span>{product.price}</span>
+                          </p>
+                          <button className={styles.order}>Add to Cart</button>
+                        </article>
+                      </motion.li>
+                    );
+                  })}
                 </ul>
               </section>
               <section className={styles['on-sale-2']}>
                 <h2 className='heading-secondary'>On Sale</h2>
                 <ul className={styles.products}>
                   {onSale.map(product => (
-                    <motion.li key={product.id} className={styles.item}>
+                    <motion.li
+                      onClick={getDocHandler}
+                      key={product.id}
+                      className={styles.item}
+                    >
                       <article className={styles.product}>
                         <Image src={product.image} height={300} width={300} />
                         <p className={styles.price}>
