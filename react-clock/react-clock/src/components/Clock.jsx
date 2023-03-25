660c63b6e25cb15d21cad512327f543b61ee5a64
Ulathi Ncube <swiftly.graphics@gmail.com>
2023-03-25 04:50:18 +0200

    fixed clock

--- react-clock/react-clock/src/components/Clock.jsx
+++ react-clock/react-clock/src/components/Clock.jsx
@@ -2,7 +2,19 @@ import React, { useEffect, useState } from 'react';
 import styles from './Clock.module.css';
 
 const Clock = () => {
-  const date = new Date();
+  const [time, setTime] = useState('00:00');
+
+  useEffect(() => {
+    setInterval(() => {
+      const date = new Date();
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+      const seconds = date.getSeconds();
+      setTime(`${hours}:${minutes}:${seconds}`);
+    }, 1000);
+  }, []);
+
+  return <div className={styles.clock}>{time}</div>;
 };
 
 export default Clock;
